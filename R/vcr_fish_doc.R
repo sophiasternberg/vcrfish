c438a635fc3d58e36e51e0ad064c282997259f1a
sophiasternberg <sophiasternberg@ucsb.edu>
2021-05-07 19:46:13 -0700

    added vignettes

---
+++ R/vcr_fish_doc.R
@@ -0,0 +1,8 @@
+#' Virginia Coast Reserve Fish Counts and Lengths
+#'
+#' Sampling of small fish in South Bay (west of Wreck Island) and Hog Island Bay was conducted using seine nets. Individual fish were identified and their length measured
+#'
+#' @source {McGlathery, K. 2019. Fish Counts and Lengths in South Bay and Hog Island Bay, Virginia 2012-2018 ver 11. Environmental Data Initiative. https://doi.org/10.6073/pasta/af32b580d4646f4b35d7552d83e9b09b (Accessed 2021-05-03).}
+#' \url{Add link for data package here}
+
+"vcr_fish"
---
+++ data-raw/vcr_fish.R
@@ -0,0 +1,21 @@
+# Attach packages
+library(usethis)
+library(metajam)
+library(dplyr)
+library(janitor)
+
+# Save link location for the data package:
+vcr_url <- "https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-vcr.236.11&entityid=27549758f3eeecd9be5344562e0340fb"
+
+# Download the data package with metajam
+vcr_download <- download_d1_data(data_url = vcr_url, path = tempdir(), dir_name="vcr_fish")
+
+# Read in data
+vcr_files <- read_d1_files(vcr_download, "read.csv",
+                           header=T
+                           ,skip=21
+                           ,sep=","
+                           ,quot='"' )
+vcr_fish <- vcr_files$data %>% clean_names()
+
+usethis::use_data(vcr_fish, overwrite = TRUE)
